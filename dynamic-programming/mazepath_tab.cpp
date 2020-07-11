09fd7883e0b84041f4599abb177e4d9c1d380b64
nitishp25 <pasrichanitish2@gmail.com>
2020-07-11 22:20:53 +0530

    mazepath tabulation

--- dynamic-programming/mazepath_tab.cpp
+++ dynamic-programming/mazepath_tab.cpp
@@ -4,10 +4,35 @@ using namespace std;
 
 int solve(int sr, int sc, int er, int ec, vector<vector<int> > &dp) {
 
-    dp[er][ec] = 1;
-
-    for(int i = er-1; i >= sr; i++) {
-        
+    for(int i = er; i >= sr; i--) {
+        for(int j = ec; j >= sc; j--) {
+            if(i == er && j == ec) {
+                dp[i][j] = 1;
+                continue;
+            }
+            for(int jump = 1; jump+j <= ec; jump++)
+                dp[i][j] += dp[i][j+jump];
+            for(int jump = 1; jump+i <= er; jump++)
+                dp[i][j] += dp[i+jump][j];
+            for(int jump = 1; jump+j <= ec && jump+i <= er; jump++)
+                dp[i][j] += dp[i+jump][j+jump];
+        }
     }
 
+    return dp[0][0];
+}
+
+int main() {
+
+    vector<vector<int> > dp;
+    vector<int> dp1;
+    for(int i = 0; i < 3; i++) {
+        for(int j = 0; j < 3; j++) {
+            dp1.push_back(0);
+        }
+        dp.push_back(dp1);
+    }
+    cout<<solve(0, 0, 2, 2, dp)<<endl;
+
+    return 0;
 }
