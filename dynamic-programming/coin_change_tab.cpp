7317c265644780fef255ceba0249c066eaa7414f
nitishp25 <pasrichanitish2@gmail.com>
2020-07-19 22:12:20 +0530

    update

--- dynamic-programming/coin_change_tab.cpp
+++ dynamic-programming/coin_change_tab.cpp
@@ -3,17 +3,25 @@
 
 using namespace std;
 
-int solve(int sum, vector<int> coins, vector<vector<int> > &dp) {
+int solve(int sum, vector<int> coins, vector<int> &dp) {
 
-
-
+    dp[0] = 1;
+    for(int i = 0; i < coins.size(); i++) {
+        for(int j = 1; j <= sum; j++) {
+            if(j - coins[i] >= 0) {
+                dp[j] += dp[j - coins[i]];
+            }
+        }
+    }
+    return dp[sum];
 }
 
 int main() {
 
     int sum = 4;
     vector<int> coins{1, 2, 3};
-    vector<vector<int> > dp(sum+1, vector<int>(4, 0));
+    vector<int> dp(sum+1, 0);
+    cout<<solve(sum, coins, dp)<<endl;
 
     return 0;
 }
