66b05f405bf876035bf09c315a29da942f36dcd4
nitishp25 <pasrichanitish2@gmail.com>
2020-07-25 21:37:47 +0530

    update

---
+++ dynamic-programming/palindromic_subseq.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int solve(string s, vector<vector<int> > dp) {
+
+    for(int i = 0; i < s.length(); i++) {
+        for(int j = i, si = 0; j < s.length(); j++, si++) {
+            if(i == 0)
+                dp[si][j] = 1;
+            else if(s[si] == s[j]) {
+                dp[si][j] += dp[si+1][j-1] + 1;
+                dp[si][j] += dp[si+1][j] + dp[si][j-1] - dp[si+1][j-1];
+            }
+            else
+                dp[si][j] += dp[si+1][j] + dp[si][j-1] - dp[si+1][j-1];
+        }
+    }
+    return dp[0][s.length() - 1];
+}
+
+int main() {
+
+    string s = "aaaa";
+    vector<vector<int> > dp(s.length(), vector<int>(s.length(), 0));
+    cout<<solve(s, dp)<<endl;
+
+    return 0;
+}
