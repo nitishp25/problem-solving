7bec557db3a9164fed305ea945454d7af4843b02
nitishp25 <pasrichanitish2@gmail.com>
2020-07-27 22:05:07 +0530

    LCS

--- DP/LCSeq_tab.cpp
+++ DP/LCSeq_tab.cpp
@@ -8,11 +8,10 @@ int solve(string a, string b, vector<vector<int> > dp) {
     for(int i = a.length() - 1; i >= 0; i--) {
         for(int j = b.length() - 1; j >= 0; j--) {
             if(a[i] == b[j]) {
-                dp[i][j] += dp[i+1][j+1] + 1;
-                dp[i][j] += dp[i+1][j] + dp[i][j+1] - dp[i+1][j+1];
+                dp[i][j] = dp[i+1][j+1] + 1;
             }
             else
-                dp[i][j] += dp[i+1][j] + dp[i][j+1] - dp[i+1][j+1];
+                dp[i][j] = max(dp[i+1][j], dp[i][j+1]);
         }
     }
     return dp[0][0];
@@ -20,7 +19,7 @@ int solve(string a, string b, vector<vector<int> > dp) {
 
 int main() {
 
-    string a = "AGGTAB", b = "GXTXAYB";
+    string a = "ABTHERC", b = "DSFRTHEBC";
     vector<vector<int> > dp(a.length() + 1, vector<int>(b.length() + 1, 0));
     cout<<solve(a, b, dp)<<endl;
 
--- DP/LCStr.cpp
+++ DP/LCStr.cpp
@@ -3,21 +3,28 @@
 
 using namespace std;
 
-int solve(string a, string b, vector<vector<int> > dp) {
+int solve(string a, string b, vector<vector<int> > &dp, int ans) {
 
-    int ans = 0;
-    for(int i = 0; i < a.length(); i++) {
-        for(int j = 0; j < b.length(); j++) {
-            if(a[i] == b[j])
-        }
+    if(a.length() == 0 || b.length() == 0)
+        return ans;
+
+    if(dp[dp.size() - a.length()][dp[0].size() - b.length()] != 0)
+        return dp[dp.size() - a.length()][dp[0].size() - b.length()];
+
+    if(a[0] == b[0]) {
+        ans = solve(a.substr(1), b.substr(1), dp, ans+1);
     }
+    ans = max(max(solve(a.substr(1), b, dp, 0), solve(a, b.substr(1), dp, 0)), ans);
 
+    dp[dp.size() - a.length()][dp[0].size() - b.length()] = ans;
+    return ans;
 }
 
 int main() {
 
     string a = "ABHRDEG", b = "FEWDEGAB";
-    vector<vector<int> > dp(a.length(), vector<int>(b.length(), 0));
+    vector<vector<int> > dp(a.length()+1, vector<int>(b.length()+1, 0));
+    cout<<solve(a, b, dp, 0)<<endl;
 
     return 0;
 }
