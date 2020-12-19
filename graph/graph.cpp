58bcb323c444ecda3d2b003191f3efa9cbc075bd
nitishp25 <pasrichanitish2@gmail.com>
2020-12-19 22:00:15 +0530

    update graph

--- graph/graph.cpp
+++ graph/graph.cpp
@@ -4,35 +4,57 @@
 
 using namespace std;
 
-void bfs(int s, vector<vector<int>> adj) {
+class Graph {
 
-    queue<int> q;
-    vector<bool> visited(10, false);
-    visited[s] = true;
-    q.push(s);
-    while(!q.empty()) {
-        int n = q.front();
-        cout<<n<<" ";
-        q.pop();
-        for(int i : adj[n]) {
-            if(!visited[i]) {
-                visited[i] = true;
-                q.push(i);
+    public:
+    int size;
+    vector<vector<int>> adj;
+
+    Graph(int s) {
+        size = size;
+        adj = vector<vector<int>>(s);
+    }
+
+    void addEdge(int v1, int v2) {
+        adj[v1].push_back(v2);
+        adj[v2].push_back(v1);
+    }
+
+    void removeEdge(int v1, int v2) {
+
+    }
+
+    void bfs(int s) {
+
+        queue<int> q;
+        vector<bool> visited(10, false);
+        visited[s] = true;
+        q.push(s);
+        while(!q.empty()) {
+            int n = q.front();
+            cout<<n<<" ";
+            q.pop();
+            for(int i : adj[n]) {
+                if(!visited[i]) {
+                    visited[i] = true;
+                    q.push(i);
+                }
             }
         }
     }
-}
 
-void dfs(int s, vector<vector<int>> adj, vector<bool> &visited) {
+    void dfs(int s, vector<bool> &visited) {
 
-    visited[s] = true;
-    cout<<s<<" ";
-    for(int i : adj[s]) {
-        if(!visited[s]) {
-            dfs(i, adj, visited);
+        visited[s] = true;
+        cout<<s<<" ";
+        for(int i : adj[s]) {
+            if(!visited[s]) {
+                dfs(i, visited);
+            }
         }
     }
-}
+
+};
 
 int main() {
 
